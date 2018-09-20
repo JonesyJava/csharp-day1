a5255af14762030db1617af7051ec7ef52118301
Jake Overall <joverall22@gmail.com>
2018-09-20 11:27:56 -0600

    Adds Collections

--- Models/User.cs
+++ Models/User.cs
@@ -4,13 +4,19 @@ namespace day_1.Models
   public class User
   {
     public string Name { get; set; }
-    private int Age { get; set; }
+    public int Age { get; private set; }
+    public bool IsAlive { get; set; }
+
+    public void Birthday()
+    {
+      Age++;
+    }
 
     public User(string name, int age)
     {
       Name = name;
       Age = age;
-
+      IsAlive = true;
     }
   }
 
